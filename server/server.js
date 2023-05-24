bc46e68e9826f70e9f4b4b9f1eceb7c552223757
sotiriosc <chortogiannoss@gmail.com>
2023-05-23 20:27:08 -0400

    fixed bug

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const db = require('./config/connection');
 
 // Set up Apollo Server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 25280;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
